7b6581551f38c703fe7b152e85df20b53257e661
Destoh2020 <desterianragira@gmail.com>
2022-12-02 01:38:09 -0800

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     print("{} argument:".format(len(argv) - 1) if len(argv) == 2 else\
-                         "{} arguments:".format(len(argv) - 1))
+      "{} arguments:".format(len(argv) - 1))
      for i in range(1, len(argv)):
          print("{}: {}".format(i, argv[i]))
