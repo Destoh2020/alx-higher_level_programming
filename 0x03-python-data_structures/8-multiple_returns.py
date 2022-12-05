1b8be508e4a29e5d2ccb2acac57a0a0dfc193589
Destoh2020 <desterianragira@gmail.com>
2022-12-05 00:47:22 -0800

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_a = ((len(sentence), sentence[0])
-        if sentence != "" else "None")
+    tuple_a = ((len(sentence), sentence[0])\
+            if sentence != "" else "None")
     return (tuple_a)
