927ee76f322b127bcd41c7417dacc035508f1940
Destoh2020 <desterianragira@gmail.com>
2022-12-19 22:00:00 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""square module"""
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """
+
+        Constructor.
+
+        Args:
+            size: the length of a square
+        """
+        self.__size = size
+
